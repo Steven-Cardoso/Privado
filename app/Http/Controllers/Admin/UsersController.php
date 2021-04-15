b9aefac29c9b5e5dcd047e41045d5ed068c9ed66
Steven <Stevensousa23@gmail.com>
2021-04-15 22:49:21 +0200

    Added edit and delete buttons on users management
    Ja edita e apaga users.

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Role;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -33,7 +34,12 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        $roles = Role::all();
+
+        return view("admin.users.edit")->with([
+            "user" => $user,
+            "roles" => $roles
+        ]);
     }
 
     /**
@@ -45,7 +51,9 @@ class UsersController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('admin.users.index');
     }
 
     /**
@@ -56,6 +64,9 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->roles()->detach();
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
     }
 }
